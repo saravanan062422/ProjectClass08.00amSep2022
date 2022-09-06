d789afa22a3f9b6885575a7b630a5cf1451dfe2c
saravanan062422 <saravanabhava33@gmail.com>
2022-09-06 12:39:08 +0530

    Project struture

---
+++ src/test/java/org/test/SampleClass.java
@@ -0,0 +1,15 @@
+package org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SampleClass {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("facebook");
+		
+	}
+
+}
